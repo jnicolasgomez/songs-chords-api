bec7b1c4b4e36e9959c80cef624ff58579dc2e62
Nicolas Gomez <jnicolasgm@outlook.com>
2026-03-12 17:57:01 -0500

    update zod deprecated function

--- src/lists/types/types.ts
+++ src/lists/types/types.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
-export const ListSchema = z.object({
+export const ListSchema = z.looseObject({
   id: z.string().optional(),
   title: z.string(),
   user_uid: z.string(),
   private: z.boolean(),
   songs: z.array(z.string()).optional(),
-}).passthrough();
+});
 
 export type List = z.infer<typeof ListSchema>;
